9bb29d468123f6f35f8b86cc709918f215ce2223
kazuya002ex <hara.ex002@gmail.com>
2020-05-29 22:01:26 +0900

    Fix news contents

---
+++ front-page.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="<?php bloginfo('description'); ?>">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <!-- <title>Design Is  Corporate Value</title> -->
+    <?php wp_title('|', true, 'right'); bloginfo('name'); ?>
+</head>
+<body>
+    <div id="home" class="big-bg">
+      <?php get_header();?>
+      <div class="home-content home-wrapper">
+        <h2>Design <br>is Corporate Value</h2>
+      </div>
+    </div>
+
+    <section id="news" class="news-wrapper">
+      <div class="title">
+        <h2>News</h2>
+        <p class="news-title kasane">News</p>
+      </div>
+
+      <?php $the_query = new WP_Query( array( 'post_type' => 'post', 'posts_per_page' => -1 ) ); ?>
+      <?php if ( $the_query->have_posts() ) : ?>
+      <div class="news-contents">
+
+        <div class="content news-content">
+          <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+            <?php echo get_the_post_thumbnail( $post_id, $size, $attr ); ?>
+            <h3><?php the_title(); ?></h3>
+            <p><?php the_content(); ?></p>
+          <?php endwhile; ?>
+        </div>
+
+        <?php wp_reset_postdata(); ?>
+      </div>
+      <?php endif; ?>
+
+    </section>
+
+
+    <section id="blog" class="blog-wrapper">
+      <div class="title">
+        <h2>Blog</h2>
+        <p class="blog-title kasane">Blog</p>
+      </div>
+
+      <div class="blog-contents">
+        <div class="content">
+          <img src="img/content.jpg" alt="#">
+          <h3>小タイトル</h3>
+          <p>文章が入ります文章が入ります文章が入ります</p>
+        </div>
+
+        <div class="content">
+          <img src="img/content.jpg" alt="#">
+          <h3>小タイトル</h3>
+          <p>文章が入ります文章が入ります文章が入ります</p>
+        </div>
+    
+        <div class="content">
+          <img src="img/content.jpg" alt="#">
+          <h3>小タイトル</h3>
+          <p>文章が入ります文章が入ります文章が入ります</p>
+        </div>
+      </div>
+    </section>
+
+    <section id="our-service" class="service-wrapper">
+      <div class="title">
+        <h2>Our Service</h2>
+        <p class="service-title kasane">Our Service</p>
+      </div>
+      
+
+      <div class="service-contents">
+        <div class="content">
+          <img src="img/content.jpg" alt="#">
+          <h3>小タイトル</h3>
+          <p>文章が入ります文章が入ります文章が入ります</p>
+        </div>
+
+        <div class="content">
+          <img src="img/content.jpg" alt="#">
+          <h3>小タイトル</h3>
+          <p>文章が入ります文章が入ります文章が入ります</p>
+        </div>
+
+        <div class="content">
+          <img src="img/content.jpg" alt="#">
+          <h3>小タイトル</h3>
+          <p>文章が入ります文章が入ります文章が入ります</p>
+        </div>
+      </div>
+    </section>
+
+    <div id="contact">
+      <div class="contact-title">
+        <h2>CONTACT</h2>
+      </div>
+
+      <div class="contact-desc">
+        <p>お仕事のご相談、ご連絡は<br>
+            こちらから承っております。まずはお気軽にご相談ください。
+        </p>
+      </div>
+
+      <div class="contact-button">
+        <p><a href="#">お問い合わせ</a></p>
+      </div>
+    </div>
+
+    <?php get_sidebar();?>
+    
+    <script src="main.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,117 +1,26 @@
+<!-- ブログの一覧画面 -->
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+    <link rel="stylesheet" href="style.css">
     <title>Design Is  Corporate Value</title>
 </head>
 <body>
     <div id="home" class="big-bg">
-      <?php get_header();?>
-      <div class="home-content home-wrapper">
-        <h2>Design <br>is Corporate Value</h2>
-      </div>
+        <?php get_header();?>
     </div>
 
-    <section id="news" class="news-wrapper">
-      <div class="title">
-        <h2>News</h2>
-        <p class="news-title kasane">News</p>
-      </div>
-
-      <div class="news-contents">
-        <div class="content news-content news-content-one">
-          <img src="img/content.jpg" alt="#">
-          <h3>小タイトル</h3>
-          <p>文章が入ります文章が入ります文章が入ります</p>
+    <section class="single-wrapper">
+        <div class="single-title">
+            <h3>ブログの一覧でぇーす</h3>
         </div>
 
-        <div class="content news-content">
-          <img src="img/content.jpg" alt="#">
-          <h3>小タイトル</h3>
-          <p>文章が入ります文章が入ります文章が入ります</p>
+        <div class="info">
+            
         </div>
-
-        <div class="content news-content">
-          <img src="img/content.jpg" alt="#">
-          <h3>小タイトル</h3>
-          <p>文章が入ります文章が入ります文章が入ります</p>
-        </div>
-      </div>
     </section>
-
-
-    <section id="blog" class="blog-wrapper">
-      <div class="title">
-        <h2>Blog</h2>
-        <p class="blog-title kasane">Blog</p>
-      </div>
-
-      <div class="blog-contents">
-        <div class="content">
-          <img src="img/content.jpg" alt="#">
-          <h3>小タイトル</h3>
-          <p>文章が入ります文章が入ります文章が入ります</p>
-        </div>
-
-        <div class="content">
-          <img src="img/content.jpg" alt="#">
-          <h3>小タイトル</h3>
-          <p>文章が入ります文章が入ります文章が入ります</p>
-        </div>
-    
-        <div class="content">
-          <img src="img/content.jpg" alt="#">
-          <h3>小タイトル</h3>
-          <p>文章が入ります文章が入ります文章が入ります</p>
-        </div>
-      </div>
-    </section>
-
-    <section id="our-service" class="service-wrapper">
-      <div class="title">
-        <h2>Our Service</h2>
-        <p class="service-title kasane">Our Service</p>
-      </div>
-      
-
-      <div class="service-contents">
-        <div class="content">
-          <img src="img/content.jpg" alt="#">
-          <h3>小タイトル</h3>
-          <p>文章が入ります文章が入ります文章が入ります</p>
-        </div>
-
-        <div class="content">
-          <img src="img/content.jpg" alt="#">
-          <h3>小タイトル</h3>
-          <p>文章が入ります文章が入ります文章が入ります</p>
-        </div>
-
-        <div class="content">
-          <img src="img/content.jpg" alt="#">
-          <h3>小タイトル</h3>
-          <p>文章が入ります文章が入ります文章が入ります</p>
-        </div>
-      </div>
-    </section>
-
-    <div id="contact">
-      <div class="contact-title">
-        <h2>CONTACT</h2>
-      </div>
-
-      <div class="contact-desc">
-        <p>お仕事のご相談、ご連絡は<br>
-            こちらから承っております。まずはお気軽にご相談ください。
-        </p>
-      </div>
-
-      <div class="contact-button">
-        <p><a href="#">お問い合わせ</a></p>
-      </div>
-    </div>
 
     <?php get_sidebar();?>
     
