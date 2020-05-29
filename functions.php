89f5ee55daebf07d667dc7c7aefdcd95f4f02b2d
kazuya002ex <hara.ex002@gmail.com>
2020-05-29 22:15:40 +0900

    Fix php file

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 function my_scripts() {
-  wp_enqueue_style( 'style-name', get_template_directory_uri() . '/css/styles.css', array(), '1.0.0', 'all' );
+  wp_enqueue_style( 'style-name', get_template_directory_uri() . 'style.css', array(), '1.0.0', 'all' );
   wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/main.js', array( 'jquery' ), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'my_scripts' );
